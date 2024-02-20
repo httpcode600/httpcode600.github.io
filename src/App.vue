4cd84dca6e7b30e37d07bfd93020c8ce81bcee0f
httpcode600 <httpcode600@gmail.com>
2024-02-20 16:52:03 +0900

    main sidebar add

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/images/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <MainSidebar></MainSidebar>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainSidebar from './components/layout/MainSidebar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainSidebar
   }
 }
 </script>
 
 <style>
+
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ffffff;
+  background-color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
+
 </style>
